780800ebd6f30be64edf80f3570be8729d3c68e9
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-12-29 11:09:05 -0800

    add -f, -F, and -r

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ recently-scrobbled tracks on [Last.fm](http://last.fm).
 
 # Options
 
-`-f`, `--follow` - Don't exit after retrieving data, but poll Last.fm for new
-tracks.  Polling frequency is controlled by the `-F` option.  
-`-F [NUM]`, `--frequency=[NUM]` - Poll Last.fm every NUM seconds.  Defaults to 5.  Implies `-f`.  
-`-n [NUM]`, `--number=[NUM]` - List NUM tracks.  
-`-v`, `--version` - Show the installed version of lastfm-tail.  
-`-h`, `--help` - Show this help dialog.  
+    Usage: ./lastfm-tail.rb [options] [USER]
+        -f, --follow                     Don't exit after retrieving data, but poll Last.fm for new tracks.  Polling frequency is controlled by the `-F` option. Implies -r.
+        -F, --frequency NUM              Poll Last.fm every NUM seconds. Defaults to 5.  Implies `-f`. 
+        -n, --number NUM                 List NUM tracks.
+        -r, --reverse                    List tracks from oldest to newest.
+        -v, --version                    Show the installed version of lastfm-tail.
+        -h, --help                       Show this help dialog.
 
--- lastfm-tail.rb
+++ lastfm-tail.rb
@@ -3,13 +3,28 @@
 # This version does not, of course, respect any of the parameters outlined in
 # the README.
 
+require 'date'
 require 'open-uri'
 require 'optparse'
 require 'xmlsimple'
 
 trackLimit = 10
+pollFrequency = nil
+reverse = false
 OptionParser.new do |opts|
 	opts.banner = "Usage: #{$0} [options] [USER]"
+	
+	opts.on('-f', '--follow', "Don't exit after retrieving data, but poll "\
+	                         +"Last.fm for new tracks.  Polling frequency "\
+	                         +"is controlled by the `-F` option. Implies -r.") do
+		pollFrequency = 5 # Magic number
+		reverse = true
+	end
+	
+	opts.on('-F', '--frequency NUM', Integer, 'Poll Last.fm every NUM seconds. '\
+	                                +'Defaults to 5.  Implies `-f`. ') do |num|
+		pollFrequency = num
+	end
 	
 	opts.on('-n', '--number NUM', Integer, 'List NUM tracks.') do |num|
 		if num > 10
@@ -18,6 +33,10 @@ OptionParser.new do |opts|
 		end
 		
 		trackLimit = num
+	end
+	
+	opts.on('-r', '--reverse', 'List tracks from oldest to newest.') do
+		reverse = true
 	end
 	
 	opts.on('-v', '--version', 'Show the installed version of lastfm-tail.') do
@@ -32,15 +51,27 @@ OptionParser.new do |opts|
 end.parse!
 
 username = ARGV[-1] || 'xiongchiamiov'
-rss = open("http://ws.audioscrobbler.com/2.0/user/#{username}/recenttracks.rss").read()
-xml = XmlSimple.xml_in rss
-items = xml['channel'][0]['item']
-items.each_with_index do |item, i|
-	if i == trackLimit
-		break
+lastTimestamp = Time.at(0)
+
+# The logic here dealing with polling is a bit dirty.
+begin
+	rss = open("http://ws.audioscrobbler.com/2.0/user/#{username}/recenttracks.rss").read()
+	xml = XmlSimple.xml_in rss
+	items = xml['channel'][0]['item']
+	items.reverse! if reverse
+	items.each_with_index do |item, i|
+		if i == trackLimit
+			break
+		end
+		
+		timestamp = Time.parse item['pubDate'][0]
+		# Is this an older item that's overlapping?
+		if !pollFrequency.nil? && lastTimestamp >= timestamp
+			next
+		end
+		
+		puts item['title']
+		lastTimestamp = timestamp
 	end
-	
-	puts item['title']
-	#puts item['pubDate']
-end
+end while !pollFrequency.nil? && sleep(pollFrequency)
 
