36f5fbd1e2473edbe83694a52020b353b2d5c29a
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-12-28 22:41:35 -0800

    add --version

--- lastfm-tail.rb
+++ lastfm-tail.rb
@@ -4,7 +4,17 @@
 # the README.
 
 require 'open-uri'
+require 'optparse'
 require 'xmlsimple'
+
+OptionParser.new do |opts|
+	opts.banner = "Usage: #{$0} [options] [USER]"
+	
+	opts.on('-v', '--version') do
+		puts 'lastfm-tail 0.1b'
+		exit 0
+	end
+end.parse!
 
 username = ARGV[-1] || 'xiongchiamiov'
 rss = open("http://ws.audioscrobbler.com/2.0/user/#{username}/recenttracks.rss").read()
